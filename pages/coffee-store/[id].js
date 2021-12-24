1e63e3c7f98ecda5c4faa1ac83cabbcbf7d77c8c
pranta-barua007 <prantabarua247@gmail.com>
2021-12-24 11:09:54 +0600

    updated meta tags

--- pages/coffee-store/[id].js
+++ pages/coffee-store/[id].js
@@ -142,6 +142,7 @@ const CoffeeStore = (initialProps) => {
     <div className={styles.layout}>
       <Head>
         <title>{name}</title>
+        <meta name="description" content={`${name} coffee shop`}/>
       </Head>
       <div className={styles.container}>
         <div className={styles.col1}>
